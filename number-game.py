b5eb72d9e5793a8ddfb92f7c9cf907469364f4ba
MihikaSingh18 <138354730+MihikaSingh18@users.noreply.github.com>
2023-07-16 20:54:34 +0530

    Add files via upload

---
+++ number-game.py
@@ -0,0 +1,41 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      singh
+#
+# Created:     16-07-2023
+# Copyright:   (c) singh 2023
+# Licence:     <your licence>
+#---------------------------------------------------------------------------
+
+
+
+print(5 + 2)
+print(5 - 2)
+print(5 * 2)
+print(5 / 2)
+print(5 // 2)
+print(5 % 2)
+print(5 ** 2)
+
+i =5
+i =i + 2
+i +=2
+i -=2
+i *=2
+
+
+
+
+result = 3 + 5 * 2
+print(result)
+
+
+number = 5
+print(number >= 4)
+print(number <= 4)
+print( not number > 4)
+print( not number < 4)
+print (number > 4 and number > 6)
+print(number > 4 or number > 6)
